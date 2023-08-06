14608dd00f8442fad619b45bccecc747b31f67b4
tomsch4fer <tomsch4fer@gmail.com>
2023-08-06 20:53:21 +0200

    aufhübschen

--- src/components/CenteredImage.js
+++ src/components/CenteredImage.js
@@ -3,8 +3,12 @@ import './CenteredImage.css';
 
 const CenteredImage = () => {
   return (
-    <div className="centered-image">
-      <img src={require('../images/t1.jpg')} alt="Centered Image" />
+    <div className="centered-image-container">
+      <p className="centered-image-text">Grüß Gott. Ich bin Tom, Hobby-Developer.</p>
+      <p className="centered-image-subtext">Schön, dass du meine Website besuchst.</p>
+      <div className="centered-image">
+        <img src={require('../images/t5.jpg')} alt="Centered" />
+      </div>
     </div>
   );
 };
